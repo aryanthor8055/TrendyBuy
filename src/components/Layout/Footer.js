f7f458d00ac93ba2fdc9ddb6cf1375b1c24db596
Aryan <aryan16072001236809@gmail.com>
2023-05-06 06:05:22 +0530

    Created Different components structure,pages and setup Routes using react-router-dom

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">All Right Reserved &copy; Aryanthor8055</h4>
     </div>
   );
 };
