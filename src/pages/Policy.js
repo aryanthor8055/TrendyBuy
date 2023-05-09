d1573fe3e6433a3f6b34d2d1694feec5d1e0d73a
Aryan <aryan16072001236809@gmail.com>
2023-05-09 12:54:28 +0530

    Completed Pages---ContactUS, About, PageNotFound and privacyPolicy

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -3,9 +3,26 @@ import Layout from "../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <div>
-      <Layout>Privacy Policy</Layout>
-    </div>
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
