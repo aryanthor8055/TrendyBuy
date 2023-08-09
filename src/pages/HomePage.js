f45f76e4e56fce1dfb1784ef305ed0abf9e56740
“aryanthor8055” <Aryan16072001236809@gmail.com>
2023-08-10 02:38:53 +0530

    .

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -181,7 +181,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
@@ -201,7 +201,7 @@ const HomePage = () => {
                   <div className="card-name-price">
                     <button
                       className="btn btn-info ms-1"
-                      onClick={() => navigate(`/product/${p.slug}`)}
+                      onClick={() => navigate(`${process.env.REACT_APP_API}/product/${p.slug}`)}
                     >
                       More Details
                     </button>
